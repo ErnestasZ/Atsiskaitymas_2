9f519a48300fb95a973df7048e8420d01a0eeb78
jugenbernt@gmail.com <jugenbernt@gmail.com>
2024-11-20 09:47:48 +0200

    fix_balance

--- Controllers/wallet_transaction.py
+++ Controllers/wallet_transaction.py
@@ -4,7 +4,7 @@ from Misc.my_logger import log_crud_operation
 
 @log_crud_operation
 def make_payment(user: User, order: Order) -> (Wallet_transaction | Exception):
-    paiment = dbp.push_db_record(Wallet_transaction, user_id=user.id, amount=order.total_amount)
+    paiment = dbp.push_db_record(Wallet_transaction, user_id=user.id, amount=order.total_amount * -1)
     
     if isinstance(paiment, Wallet_transaction):
         dbp.push_db_record(order, status="Paid")
